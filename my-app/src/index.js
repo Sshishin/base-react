60867dc9b582d084ee75dfe41dd2f396423ae4c3
Sergey Shishin <flyers32rus@gmail.com>
2022-12-14 14:08:22 +0300

    docs(script): jsx

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -2,16 +2,41 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+
+// const elem = <h2>Hello World!</h2>
+
+// const elem = React.createElement('h2', {className: 'greetings'}, 'Hello World!'); //Так можно создавать элементы без JSX (2-ой аргумент это класс)
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   elem
+// );
+
+// Старая версия
+// Куда помещаем 2 аргумента
+
+// ReactDOM.render( 
+//   <React.StrictMode> 
+//     <App />
+//   </React.StrictMode>,  //Что будет рендериться
+//   document.getElementById('root')   //Где будет рендериться
+// );
+
+// Импорты React нужны были в старых версиях в каждый файл
+
+const text = 'Hello World'    //Можно помещаться все кроме объектов
+const elem = (  //Всегда нужно оборачиать в родительский блок
+  <div>
+    <h2>Текст: {text}, сложение: {2+3}</h2>
+    <input type="text" />
+    <button tabIndex={0}>Click</button>
+  </div>    //Всегда у одиночных тегов есть самозакрывающийся тег
+  // Атрибуты пишутся в камел кейсе
+  // Классы пишутся как className
+)
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  elem
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
